200e6478271fba872b2ae62ac59cd0d450761d97
MLC <MLC@bab10101-e421-0410-a517-8ce0973de3ef>
2012-10-18 15:24:15 +0000

    creation script updated to be mysql compliant.
    git-svn-id: http://dev.lutece.paris.fr/svn/lutece/portal/trunk/plugins/technical/plugin-paybox@44643 bab10101-e421-0410-a517-8ce0973de3ef

--- src/sql/plugins/paybox/plugin/create_db_paybox.sql
+++ src/sql/plugins/paybox/plugin/create_db_paybox.sql
@@ -2,8 +2,8 @@ DROP TABLE IF EXISTS paybox_log;
 
 CREATE TABLE paybox_log (
   id bigint NOT NULL,
-  date timestamp without time zone,
-  order_reference character varying(255)
+  `date` DATETIME,
+  order_reference VARCHAR(255)
 );
 
 ALTER TABLE paybox_log ADD CONSTRAINT paybox_log_pkey PRIMARY KEY(id );
